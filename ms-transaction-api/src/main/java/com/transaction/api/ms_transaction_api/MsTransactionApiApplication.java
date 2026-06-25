7f7783e343c4856ad21e8cbbe516ce34385918bb
Conde <pablo.conde@accenture.com>
2026-06-25 11:48:58 -0300

    first commit

---
+++ ms-transaction-api/src/main/java/com/transaction/api/ms_transaction_api/MsTransactionApiApplication.java
@@ -0,0 +1,13 @@
+package com.transaction.api.ms_transaction_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransactionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransactionApiApplication.class, args);
+	}
+
+}
---
+++ ms-transaction-api/src/test/java/com/transaction/api/ms_transaction_api/MsTransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transaction.api.ms_transaction_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
