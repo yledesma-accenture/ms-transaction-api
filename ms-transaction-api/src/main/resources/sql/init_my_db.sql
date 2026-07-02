07e212e9482ba54c780ec6fa7b10760e5ada30fd
Conde <pablo.conde@accenture.com>
2026-07-02 15:13:40 -0300

    mejora en creacion base de datos

--- ms-transaction-api/src/main/resources/sql/init_my_db.sql
+++ ms-transaction-api/src/main/resources/sql/init_my_db.sql
@@ -12,16 +12,32 @@ CREATE TABLE IF NOT EXISTS accounts (
     branch_code CHAR(10),
     default_currency CHAR(3), -- ISO 4217
     is_active BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP ,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
+
+
+-- Tabla de archivos ingeridos
+CREATE TABLE IF NOT EXISTS ingested_files (  id UUID PRIMARY KEY,
+                                              file_name VARCHAR(255),
+    file_format VARCHAR(10) CHECK (file_format IN ('CSV','JSON','XML')),
+    status VARCHAR(20) CHECK (status IN ('RECEIVED','PROCESSING','COMPLETED','FAILED','DUPLICATE')),
+    checksum VARCHAR(64) UNIQUE, -- SHA-256
+    file_size_bytes BIGINT,
+    total_rows INT,
+    success_rows INT,
+    failed_rows INT,
+    uploaded_by VARCHAR(100),
+    uploaded_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP ,
+    processed_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+                              );
 
 -- Tabla de transacciones
 CREATE TABLE IF NOT EXISTS transactions (
                                             id UUID PRIMARY KEY,
                                             external_ref VARCHAR(100) UNIQUE,
-    transaction_at TIMESTAMPTZ,
-    ingested_at TIMESTAMPTZ,
+    transaction_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    ingested_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     type VARCHAR(20) CHECK (type IN ('DEBIT','CREDIT','TRANSFER','PAYMENT','REFUND','FEE')),
     status VARCHAR(20) CHECK (status IN ('PENDING','COMPLETED','FAILED','REVERSED')),
     amount NUMERIC(18,2),
@@ -33,25 +49,10 @@ CREATE TABLE IF NOT EXISTS transactions (
     created_by VARCHAR(100),
     flagged BOOLEAN DEFAULT FALSE,
     flag_reason TEXT,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
--- Tabla de archivos ingeridos
-CREATE TABLE IF NOT EXISTS ingested_files (
-                                              id UUID PRIMARY KEY,
-                                              file_name VARCHAR(255),
-    file_format VARCHAR(10) CHECK (file_format IN ('CSV','JSON','XML')),
-    status VARCHAR(20) CHECK (status IN ('RECEIVED','PROCESSING','COMPLETED','FAILED','DUPLICATE')),
-    checksum VARCHAR(64) UNIQUE, -- SHA-256
-    file_size_bytes BIGINT,
-    total_rows INT,
-    success_rows INT,
-    failed_rows INT,
-    uploaded_by VARCHAR(100),
-    uploaded_at TIMESTAMPTZ DEFAULT NOW(),
-    processed_at TIMESTAMPTZ
-    );
 
 -- Tabla de advertencias de validación
 CREATE TABLE IF NOT EXISTS transaction_validation_warnings (
@@ -59,5 +60,5 @@ CREATE TABLE IF NOT EXISTS transaction_validation_warnings (
                                                                transaction_id UUID REFERENCES transactions(id),
     warning_code VARCHAR(50),
     warning_message TEXT,
-    created_at TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
